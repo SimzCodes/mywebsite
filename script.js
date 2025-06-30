1c28ffca415c859b699ea9f912b0bc7ae8c55797
SimzCodes <gursimarkaur09112006@gmail.com>
2025-07-01 00:00:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+document.querySelector('.next').addEventListener('click', () => {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  slides[currentSlide].classList.add('active');
+});
